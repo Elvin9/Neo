52620928ae82e7b1713adbdcfeb19793b687cad6
Alon <alonh34@gmail.com>
2017-05-27 16:30:34 +0000

    add vars to layer

--- lib/layers/layer.py
+++ lib/layers/layer.py
@@ -1,11 +1,13 @@
-
 class Layer(object):
-	def _get_output(input):
-		raise NotImplemented
+    def __init__(self):
+        self.output = 0
+        self.input = 0
 
-	def _update_parameters(delta):
-		raise NotImplemented
+    def get_output(input):
+        raise NotImplemented
 
-	def _get_input_gradient(output_gradient):
-		raise NotImplemented
+    def update_parameters(delta):
+        raise NotImplemented
 
+    def get_input_gradient(output_gradient):
+        raise NotImplemented
