5e3cf456571f0c57575e8078d4d307f3373a7410
Noam <noam.pnueli@gmail.com>
2017-05-27 17:09:34 +0300

    lil' testy

--- lib/layers/sigmoid.py
+++ lib/layers/sigmoid.py
@@ -16,8 +16,8 @@ class Sigmoid(Layer):
         return self.output
 
     def get_derivative(self):
-        sub_prod = T.sub(np.ones([len(self.output), 1]), self.output)
+        sub_prod = T.sub(np.ones([self.output.shape.eval()[0], 1]), self.output)
         return math_utils.vec_elemwise(self.output, sub_prod)
 
     def get_input_gradient(self, output_gradient):
-        return math_utils.vec_elemwise(output_gradient, self.get_derivative)
+        return math_utils.vec_elemwise(output_gradient, self.get_derivative())
