ba4b3f0d0070acf0cd78738f6c4bad6cd333662c
Noam <noam.pnueli@gmail.com>
2017-05-28 20:35:49 +0300

    generalised to mini batch

--- tests/sigmoid_test.py
+++ tests/sigmoid_test.py
@@ -4,6 +4,8 @@ import gnumpy as gp
 
 x = Sigmoid()
 
-print(x.get_output(gp.garray([1, 2, 3])))
+print(x.get_output(gp.garray([[1, 2, 3],
+                              [1, 2, 3]])))
 print(x.get_derivative())
-print(x.get_input_gradient(gp.garray([2, 2, 3])))
+print(x.get_input_gradient(gp.garray([[2, 2, 3],
+                                      [2, 2, 3]])))
