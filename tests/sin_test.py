f558907bfe997cfc0ff86bfcee5811ce106c5cc6
Alon <alonh34@gmail.com>
2017-05-30 01:04:25 +0300

    sin test working good af

--- tests/sin_test.py
+++ tests/sin_test.py
@@ -5,23 +5,23 @@ from lib.loss_functions.mse import MSELoss
 from lib.models.sequential_model import SequentialModel
 import matplotlib.pyplot as plt
 
-rate = 1.5
-data_size = 2000
+rate = 0.07
+data_size = 100000
 
 model = SequentialModel(MSELoss(), rate)
-model.add_layer(Linear(1, 4, bias=True))
+model.add_layer(Linear(1, 10, bias=True))
 model.add_layer(Sigmoid())
-model.add_layer(Linear(4, 1, bias=True))
+model.add_layer(Linear(10, 1, bias=True))
 
-data_x = np.array([np.random.rand(data_size)*90])
+data_x = np.array([np.random.rand(data_size)*180])
 data_x = np.deg2rad(data_x)
 data_y = np.sin(data_x)
 
-errors = model.train(data_x, data_y, batch_size=1, error=True)
+errors = model.train(data_x, data_y, batch_size=5, error=False)
 
 print(errors)
 
-test_x = np.arange(0, np.pi/2, 0.05)
+test_x = np.arange(0, np.pi, 0.05)
 test_y = np.sin(test_x)
 pred = [model.forward(np.array([[x]])) for x in test_x]
 
