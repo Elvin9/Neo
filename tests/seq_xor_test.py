4c938e0e33833f138eedab12844d91cae342b898
Noam <noam.pnueli@gmail.com>
2017-05-30 18:13:05 +0300

    xor test

--- tests/seq_xor_test.py
+++ tests/seq_xor_test.py
@@ -3,24 +3,26 @@ import numpy as np
 from lib.layers.rectifiers import ReLU
 from lib.layers.linear import Linear
 from lib.layers.sigmoid import Sigmoid
+from lib.layers.softmax import Softmax
 from lib.loss_functions.mse import MSELoss
+from lib.loss_functions.cross_entropy import CrossEntropy
 from lib.models.sequential_model import SequentialModel
-from lib.parameter_updates import SGD
+from lib.parameter_updates import SGD, Adagrad, Momentum
 import matplotlib.pyplot as plt
 
-rate = 0.01
+rate = 0.001
 data_size = 10000
 
 model = SequentialModel(rate, MSELoss())
 
-model.add_layer(Linear(2, 2, bias=False, parameter_update=SGD()))
+model.add_layer(Linear(2, 2, bias=False, parameter_update=Momentum()))
 model.add_layer(Sigmoid())
-model.add_layer(Linear(2, 1, bias=False, parameter_update=SGD()))
-model.add_layer(Sigmoid())
+model.add_layer(Linear(2, 2, bias=False, parameter_update=Momentum()))
+model.add_layer(Softmax())
 
 
 x_data = np.array([np.random.binomial(1, 0.5, 2) for x in range(data_size)])
-y_data = np.array([[x[0] ^ x[1] for x in x_data]])
+y_data = np.array([[x[0] ^ x[1] for x in x_data], [not(x[0] ^ x[1]) for x in x_data]])
 
 x_data = x_data.T
 y_data = y_data
