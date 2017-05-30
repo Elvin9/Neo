2d613c1f429e5b9c7746bfcf4c3dcb7988c45a1f
Noam <noam.pnueli@gmail.com>
2017-05-30 12:57:11 +0300

    implemeted relu layer

--- lib/layers/ReLU.py
+++ lib/layers/ReLU.py
@@ -1,1 +1,22 @@
-__author__ = 'Alon'
+from lib.layers.layer import Layer
+
+
+class ReLU(Layer):
+    def __init__(self):
+        super().__init__()
+
+    def get_output(self, inp):
+        self.input = inp
+
+        if inp > 0:
+            return inp
+        return 0.01 * inp
+
+    def get_derivative(self):
+        if self.input > 0:
+            return 1
+        return 0.01
+
+    def get_input_gradient(self, output_gradient):
+        return output_gradient * self.get_derivative()
+
--- tests/seq_xor_test.py
+++ tests/seq_xor_test.py
@@ -1,29 +1,31 @@
 import numpy as np
+
+from lib.layers.ReLU import ReLU
 from lib.layers.linear import Linear
 from lib.layers.sigmoid import Sigmoid
 from lib.loss_functions.mse import MSELoss
 from lib.models.sequential_model import SequentialModel
+from lib.parameter_updates import SGD
 
-rate = 1
+rate = 0.01
 data_size = 2000
 
-loss = MSELoss()
-model = SequentialModel(loss, rate)
+model = SequentialModel(rate, MSELoss())
 
-model.add_layer(Linear(2, 2, bias=False))
-model.add_layer(Sigmoid())
-model.add_layer(Linear(2, 1, bias=False))
-# model.add_layer(Sigmoid())
+model.add_layer(Linear(2, 2, bias=False, parameter_update=SGD()))
+model.add_layer(ReLU())
+model.add_layer(Linear(2, 1, bias=False, parameter_update=SGD()))
+model.add_layer(ReLU())
 
 
 x_data = np.array([np.random.binomial(1, 0.5, 2) for x in range(data_size)])
-y_data = np.array([[x[0] ^ x[1] for x in x_data], [not (x[0] ^ x[1]) for x in x_data]])
+y_data = np.array([[x[0] ^ x[1] for x in x_data]])
 
 x_data = x_data.T
 y_data = y_data
 
-errors = model.train(x_data, y_data, batch_size=1, error=True)
-print(errors)
+errors = model.train(x_data, y_data, batch_size=10, error=False)
+# print(errors)
 print('\n')
 
 test_x = np.array([[1], [0]])
