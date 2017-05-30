8dc3339996707b0e4565d48f3ade1b794a5b4b5f
Noam <noam.pnueli@gmail.com>
2017-05-30 15:07:09 +0300

    added elu and leakyrelu

--- lib/layers/ReLU.py
+++ lib/layers/ReLU.py
@@ -9,8 +9,53 @@ class ReLU(Layer):
     def get_output(self, inp):
         self.input = inp
 
-        tmp_grt = gp.garray(inp) > gp.zeros(inp.shape)
+        tmp_grt = gp.garray(inp) > 0
         return gp.garray(inp) * tmp_grt
+
+    def get_derivative(self):
+        return gp.garray(self.input) > gp.zeros(self.input.shape)
+
+    def get_input_gradient(self, output_gradient):
+        return output_gradient * self.get_derivative()
+
+
+class LeakyReLU(Layer):
+    def __init__(self):
+        super().__init__()
+
+    def get_output(self, inp):
+        self.input = inp
+
+        inp = gp.garray(inp)
+
+        tmp_grt = inp < 0
+        tmp_grt2 = inp > 0
+
+        return tmp_grt * 0.01 * inp + tmp_grt2 * inp
+
+    def get_derivative(self):
+        return gp.garray(self.input) > gp.zeros(self.input.shape)
+
+    def get_input_gradient(self, output_gradient):
+        return output_gradient * self.get_derivative()
+
+
+class ELU(Layer):
+    def __init__(self, coefficient=0.01):
+        super().__init__()
+        self.coefficient = coefficient
+
+    def get_output(self, inp):
+        self.input = inp
+
+        inp = gp.garray(inp)
+
+        tmp_grt = inp < 0
+        tmp_grt2 = inp > 0
+
+        coefficient = self.coefficient * (gp.exp(inp) - gp.ones(inp.shape))
+
+        return tmp_grt * coefficient * inp + tmp_grt2 * inp
 
     def get_derivative(self):
         return gp.garray(self.input) > gp.zeros(self.input.shape)
--- tests/relu_test.py
+++ tests/relu_test.py
@@ -1,7 +1,19 @@
-from lib.layers.ReLU import ReLU
+from lib.layers.ReLU import ReLU, LeakyReLU, ELU
 import numpy as np
 
 x = ReLU()
+
+print(x.get_output(np.array([[1, -2, 3],
+                            [-5, -3, 7]])))
+print(x.get_derivative())
+
+x = LeakyReLU()
+
+print(x.get_output(np.array([[1, -2, 3],
+                            [-5, -3, 7]])))
+print(x.get_derivative())
+
+x = ELU()
 
 print(x.get_output(np.array([[1, -2, 3],
                             [-5, -3, 7]])))
