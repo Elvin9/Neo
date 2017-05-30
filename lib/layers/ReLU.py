c7394a2eae058715764124b7642333c9faeafbb2
Noam <noam.pnueli@gmail.com>
2017-05-30 14:02:03 +0300

    not leaky relu that's not logistic (hacky)

--- lib/layers/ReLU.py
+++ lib/layers/ReLU.py
@@ -1,5 +1,6 @@
 from lib.layers.layer import Layer
 import gnumpy as gp
+import numpy as np
 
 
 class ReLU(Layer):
@@ -13,15 +14,19 @@ class ReLU(Layer):
         #     return inp
         # return 0.01 * inp
 
+        tmp_grt = gp.garray(inp) > gp.zeros(inp.shape)
+        return gp.garray(inp) * tmp_grt
+
         # Logistic implementation for now :(
 
-        return gp.log(gp.ones(inp.shape) + gp.exp(inp))
+        # return gp.log(gp.ones(inp.shape) + gp.exp(inp))
 
     def get_derivative(self):
         # if self.input > 0:
         #     return 1
         # return 0.01
-        return gp.logistic(self.input)
+        # return gp.logistic(self.input)
+        return gp.garray(self.input) > gp.zeros(self.input.shape)
 
     def get_input_gradient(self, output_gradient):
         return output_gradient * self.get_derivative()
