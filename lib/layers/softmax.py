c13a0ea383bcc49e32ba5a4380776fdbc5f48d36
Alon <alonh34@gmail.com>
2017-05-30 14:07:01 +0300

    add softmax

--- lib/layers/softmax.py
+++ lib/layers/softmax.py
@@ -1,1 +1,23 @@
-__author__ = 'Alon'
+import gnumpy as gp
+import numpy as np
+from lib.layers.layer import Layer
+
+
+class Softmax(Layer):
+    def __init__(self):
+        super().__init__()
+
+    def get_output(self, inp):
+        exp_sum = gp.sum(gp.exp(inp), axis=0).as_numpy_array()
+        inv_exp_sum = gp.garray((1 / exp_sum))
+        inv_exp_sum = gp.tile(inv_exp_sum, (inp.shape[0], 1))
+
+        return gp.exp(inp) * inv_exp_sum
+
+    def get_derivative(self):
+        sub_prod = gp.ones(self.output.shape) - self.output
+        return self.output * sub_prod
+
+    def get_input_gradient(self, output_gradient):
+        return output_gradient * self.get_derivative()
+
